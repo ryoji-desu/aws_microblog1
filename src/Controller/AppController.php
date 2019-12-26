ca6637fed96e807d4768abf9634465be3fc62273
ryojikf11 <nagai.ryoji.q0@tufs.ac.jp>
2019-12-26 21:37:22 +0900

    change auth

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -41,15 +41,145 @@ class AppController extends Controller
     {
         parent::initialize();
 
+
+        $this->viewBuilder()->layout('my_layout');
+
         $this->loadComponent('RequestHandler', [
             'enableBeforeRedirect' => false,
         ]);
         $this->loadComponent('Flash');
-
+        $this->loadComponent('Auth',[
+            'authenticate' => [
+                'Form' => [
+    			'userModel' => 'Users',
+                            'fields' => [
+                                'username' => 'username',
+                                'password' => 'password'
+                            ]
+                    ]
+                ],
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            'loginRedirect' => [
+                'controller' => 'Posts',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+        ]);
         /*
          * Enable the following component for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
          */
         //$this->loadComponent('Security');
+    }
+    public function validation_empty($s = null)
+    {
+        $error = "error";
+        if ($s[1] =="" || $s[1] == " " || empty($s[1]) ) {
+            $this->set($s[0],'please enter');
+            return $error;
+        }
+        if ($s[0] !== 'email' && !preg_match('/\A[a-zA-Z0-9 ]+\z/',$s[1])) {
+            $this->set($s[0],'You are using wrong words,only number and alphabet can be accepted');
+            return $error;
+        }
+        if ($s[0] == 'username'  && mb_strlen($s[1])>=8 ||mb_strlen($s[1])<=4) {
+            $this->set($s[0],'it should be less than 8 and more than 4');
+            return $error;
+        }
+        if ($s[0] == 'password' && mb_strlen($s[1])>=8 ||mb_strlen($s[1])<=4) {
+            $this->set($s[0],'it should be less than 8 and more than 4');
+            return $error;
+        }
+    }
+    public function upload_image($image = array())
+    {
+        $image = $this->request->data['image'];
+        $error = "error";
+        try {
+            $this -> _validateUpload($image);
+        } catch (\Exception $e){
+            $this->Flash->error('Invalid Picture');
+            return $error;
+        }
+        try {
+            $ext = $this -> _validateImageType($image);
+        } catch (\Exception $e){
+            $this->Flash->error('Invalid Imagetype');
+            return $error;
+        }
+        try {
+             $path = $this -> _save($ext,$image);
+             return $path;
+        } catch (\Exception $e){
+            $this->Flash->error('cannot save');
+            return $error;
+        }
+
+    }
+    private function _validateUpload($image) {
+        $error = "error";
+        if ( !isset($image['error'])) {
+            return $error = "error";
+        }
+        if (explode("/",$image['type'])[0] != "image") {
+            return $error = "error";
+        }
+        switch ($image['error']) {
+            case UPLOAD_ERR_OK:
+            return true;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+            return $error = "error";
+            default:
+            return $error = "error";
+        }
+    }
+
+    private function _validateImageType($image)
+    {
+        $error = "error";
+        $this->_imageType = exif_imagetype($image['tmp_name']);
+        switch($this->_imageType){
+            case IMAGETYPE_GIF:
+            return 'gif';
+            break;
+            case IMAGETYPE_JPEG:
+            return 'jpeg';
+            break;
+            case IMAGETYPE_PNG:
+            return 'png';
+            break;
+            default:
+            return $error = "error";
+        }
+    }
+    private function _save($ext,$image)
+    {
+        $error = "error";
+        $this->_imageFileName = sprintf(
+            '%s_%s.%s',
+            time(),
+            sha1(uniqid(mt_rand(), true)),
+            $ext
+        );
+
+        $uploaddir = realpath(WWW_ROOT . "img/");
+        $savePath = $uploaddir . '/' .$this->_imageFileName;
+        $res = move_uploaded_file($image['tmp_name'], $savePath);
+        if ($res === false) {
+            return $error = "error";
+        }
+        return $savePath;
+    }
+    public function beforeFilter(Event $event)
+    {
+      $this->Auth->allow(['add','activate','login']);
     }
 }
